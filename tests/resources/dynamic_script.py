1c97052ae863c12c946d366b8355051c092c3a33
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2023-07-27 16:20:52 +0100

    test: add dynamic_script tests

---
+++ tests/resources/dynamic_script.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parents[0]
+env_file = "".join([Path(__file__).stem, ".env"])
+
+
+def write_to_env_file(key, value):
+    entry = "".join([key, "=", value, "\n"])
+    with open(env_file, mode="a") as file:
+        file.write(entry)
+
+
+def counter():
+    write_to_env_file("COUNTER", "60")
+
+
+def hi():
+    write_to_env_file("HELLO", "world")
+
+
+def main():
+    counter()
+    hi()
+
+
+if __name__ == "__main__":
+    SystemExit(main())
---
+++ tests/test_run_dynamic_script.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import os
+
+import main
+
+ROOT_DIR = Path(__file__).parents[0]
+RESOURCES = ROOT_DIR / "resources"
+
+
+def test_save_file_hash_db(create_os_environ):
+    genie = main.Genie(create_os_environ)
+
+    os.environ["INPUT_DYNAMIC_SCRIPT"] = "resources/dynamic_script.py"
+    genie.run_dynamic_script()
+
+    # assert pkl_file.exists()
+
+
+
+
+
