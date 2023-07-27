318fe7283cd2daf91640d01067ee6b64ca303190
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2023-07-27 13:40:57 +0100

    feat: add md5 hash file & directory methods

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """Main python script to extract the key, value pairs and render the Jinja2 template.
 """
+import hashlib
 import pickle
 from contextlib import suppress
+from pathlib import Path
 
 
 class Config:
@@ -42,3 +44,41 @@ class Genie:
             with open(Config.hash_db, 'rb') as file:
                 return pickle.load(file)
 
+    @staticmethod
+    def generate_md5_hash(file_path: Path) -> str:
+        """Generate MD5 hash for a file.
+
+        Args:
+            file_path (Path): The file on which to calculate the MD5 hash.
+
+        Returns:
+            str: The generated MD5 hash value.
+        """
+        with suppress(FileNotFoundError):
+            with open(file_path, "rb") as file:
+                md5_hash = hashlib.md5()
+                while True:
+                    data = file.read(4096)
+                    if not data:
+                        break
+                    md5_hash.update(data)
+                return md5_hash.hexdigest()
+
+    @staticmethod
+    def generate_dir_md5_hash(dir_path: Path) -> str:
+        """Generate MD5 hashes for all files within a directory, and the directory.
+
+        Args:
+            dir_path (Path): The directory & contents on which to calculate the MD5 hash.
+
+        Returns:
+            str: The generated MD5 hash value.
+        """
+        package_hash = hashlib.md5()
+
+        for path_object in dir_path.rglob('*'):
+            if path_object.is_file():
+                file_hash = Genie.generate_md5_hash(path_object)
+                package_hash.update(file_hash.encode("utf-8"))
+
+        return package_hash.hexdigest()
