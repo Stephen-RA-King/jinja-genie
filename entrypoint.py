32fff63b2cbce28cb3041e9b271dcf1ace583be2
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2023-07-28 20:37:27 +0100

    refactor: update inputs

--- entrypoint.py
+++ entrypoint.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
-
-
 import os
 
 from main import Genie
 
 
 def main():
-    genie = Genie(os.environ)
+    env_variables = os.environ
 
-    genie.use_dynamic_variables()
+    genie = Genie(os.environ)
     genie.use_env_variables()
-    genie.use_manual_variables()
-    genie.use_data_source()
+
+    if env_variables["INPUT_DYNAMIC_SCRIPT"] != "":
+        genie.use_dynamic_variables()
+
+    if env_variables["INPUT_VARIABLES"] != "":
+        genie.use_manual_variables()
+
+    if env_variables["INPUT_DATA_SOURCE"] != "":
+        genie.use_data_source()
+
     genie.render_template()
 
 
 if __name__ == "__main__":
     SystemExit(main())
-
